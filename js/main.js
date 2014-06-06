ffceba43d5dc7f5489c6e3f5553bfe4557a9963c
sbramhall <susan.bramhall@gmail.com>
2014-06-06 14:55:42 -0400

    add phonegap device specific js

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ var app = {
   
     initialize: function() {
         var self = this;
-        this.store = new WebSqlStore(function() {
+        this.store = new MemoryStore(function() {
             self.showAlert('Store Initialized', 'Info');
         });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
